f5d887850f1e7c35c82d4d34237e4b42b4f5dc76
Jpendy <Jpendy256@gmail.com>
2020-06-09 15:02:10 -0700

    finished all tests for organization routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -25,6 +25,97 @@ describe('voting app routes', () => {
   });
 
 
+  it('it creates an organization', () => {
+    return request(app)
+      .post('/api/v1/organizations')
+      .send({
+        title: 'Cool Organization',
+        description: 'Cool description',
+        imageUrl: 'Image url placeholder'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          title: 'Cool Organization',
+          description: 'Cool description',
+          imageUrl: 'Image url placeholder',
+          __v: 0
+        });
+      });
+  });
 
+  it('it gets all organization but leaves out the description', () => {
+    return Organization.create({
+      title: 'Cool Organization',
+      description: 'Cool description',
+      imageUrl: 'Image url placeholder'
+    })
+      .then(() => request(app).get('/api/v1/organizations'))
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: expect.anything(),
+          title: 'Cool Organization',
+          imageUrl: 'Image url placeholder'
+        }]);
+      });     
+  });
+
+  it('it get an organization by id with GET', () => {
+    return Organization.create({
+      title: 'Cool Organization',
+      description: 'Cool description',
+      imageUrl: 'Image url placeholder'
+    })
+      .then(organization => request(app).get(`/api/v1/organizations/${organization._id}`))
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          title: 'Cool Organization',
+          description: 'Cool description',
+          imageUrl: 'Image url placeholder',
+          __v: 0
+        });
+      });
+  });
+
+  it('it updates an organization by id with PATCH', () => {
+    return Organization.create({
+      title: 'Cool Organization',
+      description: 'Cool description',
+      imageUrl: 'Image url placeholder'
+    })
+      .then(organization => {
+        return request(app)
+          .patch(`/api/v1/organizations/${organization._id}`)
+          .send({ title: 'Super Lame Organization' });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          title: 'Super Lame Organization',
+          description: 'Cool description',
+          imageUrl: 'Image url placeholder',
+          __v: 0
+        });
+      });
+  });
+
+  it('it deletes an organization by id with DELETE', () => {
+    return Organization.create({
+      title: 'Cool Organization',
+      description: 'Cool description',
+      imageUrl: 'Image url placeholder'
+    }) 
+      .then(organization => request(app).delete(`/api/v1/organizations/${organization._id}`))
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          title: 'Cool Organization',
+          description: 'Cool description',
+          imageUrl: 'Image url placeholder',
+          __v: 0
+        });
+      });
+  });
 
 });
